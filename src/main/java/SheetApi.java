62516b3f5ce84106aee57b0b4609a97d1f96df30
jbernez <johann.bernez@gmail.com>
2017-04-09 20:44:54 +0200

    Added the spreadsheet connection

--- src/main/java/SheetApi.java
+++ src/main/java/SheetApi.java
@@ -1,9 +1,77 @@
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Johann Bernez
  */
 public class SheetApi {
 
-    public static void main(String[] args) {
-        System.out.println("Sheet-api");
+    private static HttpTransport HTTP_TRANSPORT;
+
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"), ".credentials/sheet-api");
+
+    private static final List<String> SCOPES = Arrays.asList(SheetsScopes.SPREADSHEETS_READONLY);
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Build a new authorized API client service.
+        Sheets service = getSheetsService();
+
+        // ID of the spread sheet
+        String spreadsheetId = "1dLfAagLzYVvk-zWrrxeC2kSQzekivPsVpNzEbX7LHoY";
+    }
+
+    private static Sheets getSheetsService() throws IOException {
+        Credential credential = authorize();
+        return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).build();
+    }
+
+    public static Credential authorize() throws IOException {
+        // Load client secrets.
+        InputStream in = SheetApi.class.getResourceAsStream("/sheet-api-secret.json");
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow
+                .Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(DATA_STORE_FACTORY)
+                .setAccessType("offline")
+                .build();
+
+        Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+
+        System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+
+        return credential;
     }
 }
