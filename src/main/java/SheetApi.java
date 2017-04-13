5bfd01c8a0d1150e091304fa07dc8724063117a9
Johann BERNEZ <jbernez@pictet-technologies.com>
2017-04-13 13:43:47 +0200

    Fixed server-to-server authentication, fetch a value from a cell

--- src/main/java/SheetApi.java
+++ src/main/java/SheetApi.java
@@ -3,6 +3,8 @@ import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInsta
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.compute.ComputeCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -11,12 +13,19 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.ValueRange;
+import org.apache.http.protocol.HTTP;
+import org.mortbay.util.ajax.JSON;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.logging.Logger;
 
 /**
  * @author Johann Bernez
@@ -25,18 +34,17 @@ public class SheetApi {
 
     private static HttpTransport HTTP_TRANSPORT;
 
-    private static FileDataStoreFactory DATA_STORE_FACTORY;
+    private static final String APPLICATION_NAME = "sheet-api";
 
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-    private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"), ".credentials/sheet-api");
+    private static final Set<String> SCOPES = Collections.singleton(SheetsScopes.SPREADSHEETS);
 
-    private static final List<String> SCOPES = Arrays.asList(SheetsScopes.SPREADSHEETS_READONLY);
+    private static final Logger LOG = Logger.getLogger(SheetApi.class.getSimpleName());
 
     static {
         try {
             HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
         } catch (Throwable t) {
             t.printStackTrace();
             System.exit(1);
@@ -50,44 +58,30 @@ public class SheetApi {
         // ID of the spread sheet
         String spreadsheetId = "1dLfAagLzYVvk-zWrrxeC2kSQzekivPsVpNzEbX7LHoY";
 
-        String range = "Class Data!B2:B";
-        ValueRange response = service.spreadsheets().values()
-                .get(spreadsheetId, range)
-                .execute();
-        List<List<Object>> values = response.getValues();
+        String range = "B2";
+        Sheets.Spreadsheets.Values.Get request = service.spreadsheets().values().get(spreadsheetId, range);
 
-        if (values == null || values.size() == 0) {
-            System.out.println("No data found");
+        // https://developers.google.com/sheets/api/reference/rest/v4/ValueRenderOption
+        request.setValueRenderOption("FORMATTED_VALUE");
+
+        ValueRange response = request.execute();
+        List<List<Object>> values = Optional.of(response.getValues()).orElse(Collections.emptyList());
+        if (values.isEmpty()) {
+            LOG.info("No value at the given cell");
         } else {
-            for (List row : values) {
-                for (Object o : row) {
-                    System.out.println(o);
-                }
-            }
+            LOG.info(String.format("The value is %s", values.iterator().next().iterator().next()));
         }
     }
 
     private static Sheets getSheetsService() throws IOException {
-        Credential credential = authorize();
-        return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).build();
-    }
+        GoogleCredential credential = GoogleCredential
+                .fromStream(SheetApi.class.getResourceAsStream("sheet-api-secret.json"), HTTP_TRANSPORT, JSON_FACTORY)
+                .createScoped(SCOPES);
 
-    public static Credential authorize() throws IOException {
-        // Load client secrets.
-        InputStream in = SheetApi.class.getResourceAsStream("sheet-api-secret.json");
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        credential.refreshToken();
 
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow
-                .Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(DATA_STORE_FACTORY)
-                .setAccessType("offline")
+        return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
                 .build();
-
-        Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-
-        System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-
-        return credential;
     }
 }
